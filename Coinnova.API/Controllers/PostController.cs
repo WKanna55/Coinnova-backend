b936038e3a221117198e01f71b1c5673b7b5ffdc
Diego Bejarano <diego.bejarano@tecsup.edu.pe>
2025-05-14 10:08:08 -0500

    refactor(PostController): authorize standard instead of admin

--- Coinnova.API/Controllers/PostController.cs
+++ Coinnova.API/Controllers/PostController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Coinnova.API.Controllers;
 
 [ApiController]
-[Authorize(Roles="admin")]
+[Authorize(Roles="standard")]
 [Route("api/[controller]")]
 public class PostController : ControllerBase
 {
