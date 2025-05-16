0191af0e6386fc0e25c184ffec00c0f9134e8ba6
Diego Bejarano <diego.bejarano@tecsup.edu.pe>
2025-05-16 09:49:56 -0500

    refactor: add migration to upload AuthProvider field in User

---
+++ Coinnova.Infrastructure/Migrations/20250516140529_AddAuthProviderToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Coinnova.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthProviderToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthProvider",
+                table: "User",
+                type: "character varying(15)",
+                nullable: false,
+                defaultValue: "Local");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthProvider",
+                table: "User");
+        }
+    }
+}
