cf8ab4f603819f2ebea88de9e71e70fa068f0ad4
Kana Laptop <willian.kana@tecsup.edu.pe>
2025-06-17 11:57:17 -0500

    refactor(cors): agregando cors de coinnova frontend

--- Coinnova.API/Program.cs
+++ Coinnova.API/Program.cs
@@ -47,7 +47,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("https://coinnova-ts.vercel.app/")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
